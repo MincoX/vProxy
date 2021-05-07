902ccf0a8bcc86ef37af54da812a3fbafc0ce0e6
MincoX <903444601@qq.com>
2021-05-07 21:34:12 +0800

    Update main-prod.js

--- src/main-prod.js
+++ src/main-prod.js
@@ -7,7 +7,8 @@ import VueSocketIO from 'vue-socket.io'
 Vue.use(VueResource)
 Vue.config.productionTip = false
 Vue.http.options.emulateJSON = true
-Vue.http.options.root = 'http://proxy.mincox.club/'
+// Vue.http.options.root = 'http://proxy.mincox.club/'
+Vue.http.options.root = 'http://47.102.134.101:19999/'
 Vue.use(new VueSocketIO({
   debug: false,
   connection: 'http://47.102.134.101:19999/',
