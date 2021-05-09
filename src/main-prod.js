e6a14faae70a9bf90b1022434c87310990d80e4d
MincoX <903444601@qq.com>
2021-05-09 10:41:52 +0800

    update

--- src/main-prod.js
+++ src/main-prod.js
@@ -8,10 +8,10 @@ Vue.use(VueResource)
 Vue.config.productionTip = false
 Vue.http.options.emulateJSON = true
 // Vue.http.options.root = 'http://proxy.mincox.club/'
-Vue.http.options.root = 'http://47.102.134.101:19999/'
+Vue.http.options.root = 'http://47.102.134.101:10001/'
 Vue.use(new VueSocketIO({
   debug: false,
-  connection: 'http://47.102.134.101:19999/',
+  connection: 'http://47.102.134.101:10001/',
 }))
 
 new Vue({
